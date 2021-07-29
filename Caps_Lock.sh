9e4f36dc4831fe1d809b9f013650bb8b28127290
Kratosgp <87536015+Kratosgp@users.noreply.github.com>
2021-07-29 10:50:55 +0530

    Update Caps_Lock.sh

--- Caps_Lock.sh
+++ Caps_Lock.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 capsoff() {
+   if [ -n "${DISPLAY+x}" ]; then
    xmodmap -e "keycode 66 = NoSymbol"
+   fi
    xdotool key Caps_Lock
    notify-send 'Caps off'
    kdialog --passivepopup 'Caps off' 5
