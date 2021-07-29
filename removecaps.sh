db3295440c3e7a6a7dccdcdfb9c4af68b4d6cbf5
Kratosgp <87536015+Kratosgp@users.noreply.github.com>
2021-07-29 09:43:11 +0530

    Update removecaps.sh

--- removecaps.sh
+++ removecaps.sh
@@ -6,11 +6,11 @@ sudo cp Caps_Lock.sh /usr/bin/
 
 sudo cp caps.service /etc/systemd/system/
 
+sudo systemctl daemon-reload
+
 sudo systemctl start caps.service
 
 sudo systemctl enable caps.service
-
-sudo systemctl daemon-reload
 
 read -p "Do you wish to reboot now?(Y/n): " Input
 
